4788cdd375d02cd0fa56f7261ca488f9ac6ef957
dridri85 <adrirando@gmail.com>
2026-01-09 14:46:30 +0100

    Update 20260109100000-create-user-admin.js

--- app-annonces/src/seeders/20260109100000-create-user-admin.js
+++ app-annonces/src/seeders/20260109100000-create-user-admin.js
@@ -5,17 +5,17 @@ require('dotenv').config({quiet: true});
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    const hashpassword = await bcrypt.hash("MotDePasse123", parseInt(process.env.SALT));
+    const hashpassword = await bcrypt.hash("Adrien", parseInt(process.env.SALT));
     await queryInterface.bulkInsert('Users', [{
       firstname: "Adrien",
       lastname: "RANDONNET",
-      username: "contact@arsdv.site",
+      username: "Admin",
       password: hashpassword,
       role: 'admin'
     }], {});
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Users',{ username: "contact@arsdv.site" });
+    await queryInterface.bulkDelete('Users',{ username: "Admin" });
   }
 };

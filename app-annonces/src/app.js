22e1bd788e00a9e4cb2b6d6c6cf4eb856b94e776
dridri85 <adrirando@gmail.com>
2026-01-09 12:31:32 +0100

    Update app.js

--- app-annonces/src/app.js
+++ app-annonces/src/app.js
@@ -6,8 +6,6 @@ const YAML = require('yamljs');
 const path = require('path');
 require('dotenv').config({quiet: true});
 
-const PORT = process.env.PORT || 3015;
-
 const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
 
 app.use(express.json());
@@ -15,12 +13,5 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 initRoutes(app);
-
-// Only start server if not in test mode
-if (process.env.NODE_ENV !== 'test') {
-    app.listen(PORT, () => {
-        console.log('Server running on port', PORT);
-    });
-}
 
 module.exports = app;

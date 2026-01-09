d10a1868430ce89f6de86f6e866c5ff656a9ce1e
dridri85 <adrirando@gmail.com>
2026-01-09 10:32:27 +0100

    added swagger doc

--- app-annonces/src/app.js
+++ app-annonces/src/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 const initRoutes = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
 require('dotenv').config({quiet: true});
+
 const PORT = process.env.PORT;
 
+// Load Swagger documentation
+const swaggerDocument = YAML.load(path.join(__dirname, '../../docs/swagger.yaml'));
+
 app.use(express.json());
+
+// Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 initRoutes(app);
 
